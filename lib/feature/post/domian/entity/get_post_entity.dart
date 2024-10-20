006e431b7c2ef577bb6c57ba39a995ee895c3c93
Mohamed Taha Hassen <mt337848@gmail.com>
2024-10-20 19:30:09 +0300

    get post entity done

---
+++ lib/feature/post/domian/entity/get_post_entity.dart
@@ -0,0 +1,16 @@
+class GetPostEntity {
+  final int userId;
+
+  final int id;
+
+  final String title;
+
+  final String body;
+
+  GetPostEntity({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+}
